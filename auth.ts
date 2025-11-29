7da93f2d9c09b99cc4a3659346f2e4ffd7367e19
devvMuhammad <muhammadaljoufi@gmail.com>
2025-11-29 12:50:41 +0500

    feat: database hooks (to init username before creation) + additional types to user model

--- auth.ts
+++ auth.ts
@@ -1,5 +1,6 @@
 import { betterAuth } from "better-auth";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
+import { createAuthMiddleware } from "better-auth/api";
 import { MongoClient } from "mongodb";
 
 const mongodbUri = process.env.MONGODB_URI;
@@ -17,6 +18,33 @@ if (!clientBaseUrl) {
 
 export const auth = betterAuth({
   database: mongodbAdapter(db),
+  "databaseHooks": {
+    user: {
+      create: {
+        before: async (user) => {
+          return {
+            data: {
+              // Ensure to return Better-Auth named fields, not the original field names in your database.
+              ...user,
+              username: user.email.split("@")[0],
+            },
+          };
+        }
+      }
+    }
+  },
+  user: {
+    additionalFields: {
+      username: {
+        type: "string",
+        defaultValue: "",
+      },
+      aura: {
+        type: "number",
+        defaultValue: 0,
+      }
+    }
+  },
   emailAndPassword: {
     enabled: true,
   },
